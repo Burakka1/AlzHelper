ec2d9434f5b21afbaf663d5ab85983159c13e286
Burakka1 <bkburak45@gmail.com>
2023-04-28 13:18:45 +0300

    navbar eklende familyrelations widgeti ve sayfası oluşturuldu

---
+++ lib/FamilyRelations.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'Classes.dart';
+
+class FamilyRelations extends StatelessWidget {
+  const FamilyRelations({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                "Aile Yakınları",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FamilyRelationsWidget(
+                  name: "İsim 1",
+                  degreeOfCloseness: "Yakınlık Derecesi 1",
+                ),
+                FamilyRelationsWidget(
+                  name: "İsim 2",
+                  degreeOfCloseness: "Yakınlık Derecesi 2",
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FamilyRelationsWidget(
+                  name: "İsim 3",
+                  degreeOfCloseness: "Yakınlık Derecesi 3",
+                ),
+                FamilyRelationsWidget(
+                  name: "İsim 4",
+                  degreeOfCloseness: "Yakınlık Derecesi 4",
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Navbar.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+import 'Home.dart';
+import 'Profile.dart';
+
+
+
+class Navbar extends StatefulWidget {
+  @override
+  _NavbarState createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int currentTab = 0;
+  final List<Widget> screens = [
+    Home(),
+    Profile(),
+  ];
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = Home();
+
+  @override
+  Widget build(BuildContext context) {
+    return navbar();
+  }
+
+  Scaffold navbar() {
+    return Scaffold(
+      body: PageStorage(
+        child: currentScreen,
+        bucket: bucket,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: Icon(
+          Icons.warning_amber_sharp,
+          color: Colors.black,
+        ),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.grey.shade800,
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Home();
+                        currentTab = 0;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.home_outlined,
+                          size: 35,
+                          color: currentTab == 0 ? Colors.white : Colors.grey,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Profile();
+                        currentTab = 1;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.person_outlined,
+                          size: 35,
+                          color: currentTab == 1 ? Colors.white : Colors.grey,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
