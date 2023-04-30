c9c3fb13731b2766a0c2eadc17d238e415156cc7
Burakka1 <bkburak45@gmail.com>
2023-04-30 17:46:10 +0300

    Reminders sayfası oluşturuldu + butonu için class oluşturuldu

---
+++ lib/Reminders.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'Classes.dart';
+
+class Reminders extends StatelessWidget {
+  const Reminders({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+   return Scaffold(
+      appBar: AppBar(
+        
+      ),
+      body: Center(
+        
+      ),
+      floatingActionButton: CustomFloatingActionButton(
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+}
